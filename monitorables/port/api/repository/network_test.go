e48903f8d8767f8b10feeec1abb3a984f1a52a7d
Lost in Paradise <43679814+gotlostinparadise@users.noreply.github.com>
2025-07-24 17:56:43 +0600

    Improve port monitor with payload response detection

--- monitorables/port/api/repository/network_test.go
+++ monitorables/port/api/repository/network_test.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"errors"
+	"io"
 	"testing"
+	"time"
 
 	"github.com/monitoror/monitoror/monitorables/port/config"
 	pkgNet "github.com/monitoror/monitoror/pkg/net"
@@ -29,16 +31,42 @@ func initRepository(t *testing.T, dialer pkgNet.Dialer) *portRepository {
 func TestRepository_OpenSocket_Success(t *testing.T) {
 	mockConn := new(mocks.Conn)
 	mockConn.On("Close").Return(nil)
+	mockConn.On("SetDeadline", AnythingOfType("time.Time")).Return(nil)
+	mockConn.On("Read", Anything).Return(1, nil)
 	mockDialer := new(mocks.Dialer)
 	mockDialer.On("Dial", AnythingOfType("string"), AnythingOfType("string")).Return(mockConn, nil)
 
 	repository := initRepository(t, mockDialer)
 	if repository != nil {
-		assert.NoError(t, repository.OpenSocket("test", 1234, "tcp"))
+		responding, _, err := repository.OpenSocket("test", 1234, "tcp", nil)
+		assert.NoError(t, err)
+		assert.True(t, responding)
 		mockConn.AssertNumberOfCalls(t, "Close", 1)
 		mockConn.AssertExpectations(t)
 		mockDialer.AssertNumberOfCalls(t, "Dial", 1)
 		mockDialer.AssertExpectations(t)
+	}
+}
+
+func TestRepository_OpenSocket_Success_WithPayload(t *testing.T) {
+	mockConn := new(mocks.Conn)
+	mockConn.On("Close").Return(nil)
+	mockConn.On("SetDeadline", AnythingOfType("time.Time")).Return(nil)
+	payload := []byte{0x01, 0x02}
+	mockConn.On("Write", payload).Return(len(payload), nil)
+	mockConn.On("Read", Anything).Return(1, nil)
+
+	mockDialer := new(mocks.Dialer)
+	mockDialer.On("Dial", AnythingOfType("string"), AnythingOfType("string")).Return(mockConn, nil)
+
+	repository := initRepository(t, mockDialer)
+	if repository != nil {
+		responding, _, err := repository.OpenSocket("test", 1234, "udp", payload)
+		assert.NoError(t, err)
+		assert.True(t, responding)
+		mockConn.AssertCalled(t, "Write", payload)
+		mockConn.AssertNumberOfCalls(t, "Close", 1)
+		mockDialer.AssertNumberOfCalls(t, "Dial", 1)
 	}
 }
 
@@ -48,8 +76,28 @@ func TestRepository_OpenSocket_Failed(t *testing.T) {
 
 	repository := initRepository(t, mockDialer)
 	if repository != nil {
-		assert.Error(t, repository.OpenSocket("test", 1234, "tcp"))
+		_, _, err := repository.OpenSocket("test", 1234, "tcp", nil)
+		assert.Error(t, err)
 		mockDialer.AssertNumberOfCalls(t, "Dial", 1)
 		mockDialer.AssertExpectations(t)
+	}
+}
+
+func TestRepository_OpenSocket_NoResponse(t *testing.T) {
+	mockConn := new(mocks.Conn)
+	mockConn.On("Close").Return(nil)
+	mockConn.On("SetDeadline", AnythingOfType("time.Time")).Return(nil)
+	mockConn.On("Read", Anything).Return(0, io.EOF)
+
+	mockDialer := new(mocks.Dialer)
+	mockDialer.On("Dial", AnythingOfType("string"), AnythingOfType("string")).Return(mockConn, nil)
+
+	repository := initRepository(t, mockDialer)
+	if repository != nil {
+		responding, _, err := repository.OpenSocket("test", 1234, "tcp", nil)
+		assert.NoError(t, err)
+		assert.True(t, responding)
+		mockConn.AssertNumberOfCalls(t, "Close", 1)
+		mockDialer.AssertNumberOfCalls(t, "Dial", 1)
 	}
 }
